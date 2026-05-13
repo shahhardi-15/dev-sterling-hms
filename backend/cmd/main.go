a07082e16d524d8011be4c711e1e1bae73f9b95c
Hardi Shah <hardishah1505@gmail.com>
2026-05-13 11:15:09 +0530

    Update API URL and CORS for production

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -48,7 +48,7 @@ func main() {
 
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://localhost:5174"},
+		AllowOrigins:     []string{"http://localhost:5174", "http://localhost:5173", "https://dev-sterling-hms.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},

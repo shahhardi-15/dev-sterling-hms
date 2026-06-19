4d59b035b57e33a618c12dc2a8f2d9daa533d381
Hardi Shah <hardishah1505@gmail.com>
2026-06-20 01:39:56 +0530

    receptionist dashboard mobile view

--- frontend/src/views/receptionist/ReceptionistAppointments.vue
+++ frontend/src/views/receptionist/ReceptionistAppointments.vue
@@ -2,7 +2,9 @@
   <div>
     <div class="mb-8 flex items-center justify-between">
       <div>
-        <h1 class="text-3xl font-bold text-gray-900">Appointments</h1>
+        <h1 class="text-2xl sm:text-3xl font-bold text-gray-900">
+          Appointments
+        </h1>
         <p class="text-gray-400 text-sm mt-1">
           View and manage all appointments
         </p>
